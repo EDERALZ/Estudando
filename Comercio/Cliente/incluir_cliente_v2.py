03daac38479e5a4c63d70131ad20f9de8796a647
edson.almeida <edson.almeida@luizalabs.com>
2023-01-27 06:35:04 -0500

    Atualização

--- Comercio/Cliente/incluir_cliente_v2.py
+++ Comercio/Cliente/incluir_cliente_v2.py
@@ -57,16 +57,44 @@ def verificar_digito(entrada):
     #     mensagem_cep()
     return saida
 
+
 def verificar_cep(clientes,cadastro,cep_valido):
     if cep_valido == True:
+        verificar_senha(cadastro)
         escrever_informacoes(clientes, cadastro)
         gravar_informacoes(clientes)
     else:
         mensagem_cep()
 
 
+def mensagem_senha():
+    print("Senha invalida, tente novamente")
+    incluir_cliente_v2()
+
+def verificar_senha(cadastro):
+    contador_minuscula = 0
+    contador_maiuscula = 0
+    contador_digito = 0
+    contador_simbolo = 0
+    if (len(cadastro['Senha']) >= 6):
+        for i in cadastro['Senha']:
+            if (i.islower()):
+                contador_minuscula += 1
+            if (i.isupper()):
+                contador_maiuscula += 1
+            if (i.isdigit()):
+                contador_digito += 1
+            if (i == '@' or i == '$' or i == '_'):
+                contador_simbolo += 1
+    if (contador_minuscula >= 1 and contador_maiuscula >= 1 and contador_digito >= 1 and contador_simbolo >= 1
+            and contador_minuscula + contador_maiuscula + contador_digito + contador_simbolo == len(cadastro['Senha'])):
+        print("Senha valida")
+    else:
+        mensagem_senha()
+
 def mensagem_cep():
     print("cep informado não existe, tente novamente")
+    incluir_cliente_v2()
 
 def mensagem_erro():
     print("Informação errada", "retornando ao início")
