fa00b3b464b516ad4714547cf6d66415ba5e6f44
edson.almeida <edson.almeida@luizalabs.com>
2023-01-27 06:35:35 -0500

    att

---
+++ Comercio/Cliente/tem2p.py
@@ -0,0 +1,8 @@
+#vetor = [0,1,2]
+#for item in vetor:
+    #print(item)
+
+
+vetor = range(50)
+for item in vetor:
+    print(item)
--- Comercio/Cliente/temporario.py
+++ Comercio/Cliente/temporario.py
@@ -1,16 +1,18 @@
-a, b, c, d, e = 0, 0, 0, 0, 0
+a, b, c, d = 0, 0, 0, 0
 senha = input("Digite uma senha: ")
+
 if (len(senha) >= 6):
     for i in senha:
         if (i.islower()):
-            a+=1
+            a += 1
         if (i.isupper()):
-            b+=1
-        if (i.isdigit()):
-            c+=1
-        if(i=='@'or i=='$' or i=='_'):
-            d+=1
-if (a>=1 and b>=1 and c>=1 and d>=1 and a+b+c+d==len(senha)):
+            b += 1
+        if (i.isdigit() or i.isnumeric()):
+            c += 1
+        if(i == '@' or i == '$' or i == '_'):
+            d += 1
+
+if (a >= 1 and b >= 1 and c >= 1 and d >= 1 and a+b+c+d == len(senha)):
     print("Senha valida")
 else:
     print("Senha invalida")
